a75e03d707cb16ed80552e1fc7aa3d3bc9284a91
Mohamad Zolfaghari Pour <zolfagharipour@gmail.com>
2026-03-18 15:29:20 +0100

    move engine is done, requires testing

---
+++ incs/cubie.h
@@ -0,0 +1,55 @@
+#ifndef CUBIE_H
+#define CUBIE_H
+
+#include <array>
+#include <cstdint>
+#include <stdexcept>
+
+enum Move {
+    U, U2, U_PRIME,
+    D, D2, D_PRIME,
+    L, L2, L_PRIME,
+    R, R2, R_PRIME,
+    F, F2, F_PRIME,
+    B, B2, B_PRIME
+};
+
+enum Corner
+{
+    URF = 0,
+    UFL = 1,
+    ULB = 2,
+    UBR = 3,
+    DFR = 4,
+    DLF = 5,
+    DBL = 6,
+    DRB = 7
+};
+
+enum Edge
+{
+    UR = 0,
+    UF = 1,
+    UL = 2,
+    UB = 3,
+    DR = 4,
+    DF = 5,
+    DL = 6,
+    DB = 7,
+    FR = 8,
+    FL = 9,
+    BL = 10,
+    BR = 11
+};
+
+struct Cubie {
+    uint8_t corner_perm[8];
+    uint8_t corner_ori[8];
+    uint8_t edge_perm[12];
+    uint8_t edge_ori[12];
+};
+
+void apply_move(Cubie& cube, Move move);
+Cubie after_move(Cubie const& cube, Move move);
+
+#endif
