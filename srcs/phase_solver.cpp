b154f9514c51824f49b2e7ad51ad048c13ee40a7
Mohamad Zolfaghari Pour <zolfagharipour@gmail.com>
2026-03-18 22:00:37 +0100

    IDA is generic now

--- srcs/phase_solver.cpp
+++ srcs/phase_solver.cpp
@@ -6,6 +6,10 @@ static bool is_valid_move(Move move, Move last_move) {
     return move_group != last_move_group;
 }
 
+size_t Thistlethwaite::get_solution_length() const {
+    return _path.size();
+}
+
 bool Thistlethwaite::is_phase_1_complete(const Cubie& cube) const {
     return is_phase_1_solved(cube);
 }
@@ -19,24 +23,24 @@ bool Thistlethwaite::is_phase_1_solved(const Cubie& cube) const {
     return true;
 }
 
-bool Thistlethwaite::dfs_phase_1(const Cubie& cube, int depth, int limit, std::vector<std::string>& path, Move last_move) {
-    int h = _eo_prune[encodeEO(cube)];
+bool Thistlethwaite::dfs(const Cubie& cube, const PhaseRules& rules, int depth, int limit, std::vector<std::string>& path, Move last_move) {
+    int h = rules.heuristic(cube);
 
-    if (is_phase_1_solved(cube))
+    if (rules.is_goal(cube))
         return true;
 
     if (depth + h > limit)
         return false;
 
-    for (int i = 0; i < 18; ++i) {
-        Move move = static_cast<Move>(i);
+    for (int i = 0; i < rules.move_count; ++i) {
+        Move move = rules.moves[i];
         if (!is_valid_move(move, last_move))
             continue;
         Cubie next = after_move(cube, move);
 
         path.push_back(move_to_string(move));
 
-        if (dfs_phase_1(next, depth + 1, limit, path, move))
+        if (dfs(next, rules, depth + 1, limit, path, move))
             return true;
 
         path.pop_back();
@@ -44,17 +48,12 @@ bool Thistlethwaite::dfs_phase_1(const Cubie& cube, int depth, int limit, std::v
     return false;
 }
 
-size_t Thistlethwaite::get_solution_length() const {
-    return _path.size();
-}
-
-bool Thistlethwaite::solve_phase_1(const Cubie& cube) {
-    int limit = _eo_prune[encodeEO(cube)];
-    Move    m = NOMOVE;
+bool Thistlethwaite::solve_phase(const Cubie& cube, const PhaseRules& rules) {
+    int limit = rules.heuristic(cube);
 
     while (true) {
         _path.clear();
-        if (dfs_phase_1(cube, 0, limit, _path, m))
+        if (dfs(cube, rules, 0, limit, _path, NOMOVE))
             return true;
         ++limit;
         if (limit > 50)
